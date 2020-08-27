820db94751da6029639546d859e7f3690528093e
Dan <dan.mok999@yandex.ru>
2020-08-27 16:47:55 +0700

    UI props

--- HelloEvolution/EmulationView/MainWindow.xaml.cs
+++ HelloEvolution/EmulationView/MainWindow.xaml.cs
@@ -25,32 +25,8 @@ namespace EmulationView
 			InitializeComponent();
 		}
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void PrepareEmulation(object sender, RoutedEventArgs e)
         {
-			var mapSize = new Size(10, 10);
-			var cellSize = new Size(24, 24);
-			for (var i = 0; i < mapSize.Width; i++)
-			{
-				var value = new ColumnDefinition();
-				value.Width = new GridLength(cellSize.Width);
-				MapField.ColumnDefinitions.Add(value);
-			}
-			for (var i = 0; i < mapSize.Height; i++)
-			{
-				var value = new RowDefinition();
-				value.Height = new GridLength(cellSize.Height);
-				MapField.RowDefinitions.Add(value);
-			}
-			var button = new Button();
-			button.Width = cellSize.Width;
-			button.Height = cellSize.Height;
-			button.Content = "00";
-			MapField.Children.Add(button);
-			var button2 = new Button();
-			button2.Width = cellSize.Width;
-			button2.Height = cellSize.Height;
-			button2.Content = "02";
-			MapField.Children.Add(button2);
-		}
-    }
+        }
+	}
 }
