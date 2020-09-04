c14c9a7b1b4183117e95e7bdac26ea9d03482599
Dan <dan.mok999@yandex.ru>
2020-09-05 00:20:44 +0500

    Added new propGrid element

---
+++ HelloEvolution/EmulationView/ViewModels/EmulationConfigViewModel.cs
@@ -0,0 +1,17 @@
+﻿using EmulationModel;
+using Syncfusion.Windows.PropertyGrid;
+
+namespace EmulationView.ViewModels
+{
+    public class EmulationConfigViewModel
+    {
+        public object EmulationConfig { get; set; }
+        public PropertyExpandModes PropertyExpandMode { get; set; }
+
+        public EmulationConfigViewModel(EmulationConfig config)
+        {
+            EmulationConfig = config;
+            PropertyExpandMode = PropertyExpandModes.FlatMode;
+        }
+    }
+}
